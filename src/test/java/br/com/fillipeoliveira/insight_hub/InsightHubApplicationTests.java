80554c8f0725f5a03f09c063b98c9b28780aac14
Fillipe Oliveira <fillipe.andrade20@gmail.com>
2024-12-06 22:46:33 -0300

    first commit

---
+++ src/test/java/br/com/fillipeoliveira/insight_hub/InsightHubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fillipeoliveira.insight_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
