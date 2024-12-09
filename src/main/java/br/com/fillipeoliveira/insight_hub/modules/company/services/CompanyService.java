ef15fcdb3f24664b1193ed27df09f5e3ab3eb5f6
Fillipe Oliveira <fillipe.andrade20@gmail.com>
2024-12-09 16:19:37 -0300

    adicionando validação ao service

--- src/main/java/br/com/fillipeoliveira/insight_hub/modules/company/services/CompanyService.java
+++ src/main/java/br/com/fillipeoliveira/insight_hub/modules/company/services/CompanyService.java
@@ -3,6 +3,7 @@ package br.com.fillipeoliveira.insight_hub.modules.company.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.fillipeoliveira.insight_hub.modules.company.exceptions.CompanyAlreadyExistsException;
 import br.com.fillipeoliveira.insight_hub.modules.company.models.entities.Company;
 import br.com.fillipeoliveira.insight_hub.modules.company.models.repositories.CompanyRepository;
 
@@ -13,6 +14,11 @@ public class CompanyService {
   private CompanyRepository companyRepository;
 
   public Company save(Company company) {
+    this.companyRepository.findByCnpj(company.getCnpj())
+        .ifPresent(existingCompany -> {
+            throw new CompanyAlreadyExistsException();
+        });
+
     return this.companyRepository.save(company);
   }
 }
