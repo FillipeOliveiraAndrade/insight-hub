d630756bca695b210d6f0b8a213c1030db296484
Fillipe Oliveira <fillipe.andrade20@gmail.com>
2024-12-11 00:10:35 -0300

    relacionando company com product

--- src/main/java/br/com/fillipeoliveira/insight_hub/modules/company/models/entities/Company.java
+++ src/main/java/br/com/fillipeoliveira/insight_hub/modules/company/models/entities/Company.java
@@ -1,21 +1,30 @@
 package br.com.fillipeoliveira.insight_hub.modules.company.models.entities;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import br.com.fillipeoliveira.insight_hub.modules.product.models.entities.Product;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "company")
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Company {
   
   @Id
@@ -26,6 +35,10 @@ public class Company {
   private String email;
   private String cnpj;
   private String password;
+
+  @OneToMany(mappedBy = "company")
+  @JsonIgnore
+  private List<Product> products;
 
   @UpdateTimestamp
   private LocalDateTime updatedAt;
