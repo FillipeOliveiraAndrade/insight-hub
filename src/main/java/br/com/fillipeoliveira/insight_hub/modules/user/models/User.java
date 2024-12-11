48f5eb507aacb25fc0759e9963787da876cde168
Fillipe Oliveira <fillipe.andrade20@gmail.com>
2024-12-11 00:18:15 -0300

    criando entidade de usuários

---
+++ src/main/java/br/com/fillipeoliveira/insight_hub/modules/user/models/User.java
@@ -0,0 +1,38 @@
+package br.com.fillipeoliveira.insight_hub.modules.user.models;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "user")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  private String username;
+  private String email;
+  private String password;
+
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+}
