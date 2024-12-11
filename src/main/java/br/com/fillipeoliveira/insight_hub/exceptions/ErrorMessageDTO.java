2c6be23cdd83145a00c26c07de2bed20e859ff4f
Fillipe Oliveira <fillipe.andrade20@gmail.com>
2024-12-11 00:11:03 -0300

    mudando exceptions de diretorio

---
+++ src/main/java/br/com/fillipeoliveira/insight_hub/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,12 @@
+package br.com.fillipeoliveira.insight_hub.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+
+  private String message;
+  private String field;
+}
